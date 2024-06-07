73a29790a62cf670ce3690a50f42c0bdddcf6e87
tommic81 <tom.michnik@gmail.com>
2024-06-07 15:12:50 +0200

    Reverting to the initial project

--- ssc-brewery/src/main/java/guru/sfg/brewery/repositories/security/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package guru.sfg.brewery.repositories.security;
-
-import guru.sfg.brewery.domain.security.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Integer> {
-}
